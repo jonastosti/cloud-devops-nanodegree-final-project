35450d23eed5f2e71c9d4b5da599e61570d6959a
Jonas <jonas.tosti@telefonica.com>
2021-11-06 17:55:43 -0300

    Adding Initial Files

---
+++ src/main/java/com/clouddevopsnanodegreefinalproject/CloudDevopsNanodegreeFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.clouddevopsnanodegreefinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDevopsNanodegreeFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDevopsNanodegreeFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clouddevopsnanodegreefinalproject/CloudDevopsNanodegreeFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clouddevopsnanodegreefinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDevopsNanodegreeFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
