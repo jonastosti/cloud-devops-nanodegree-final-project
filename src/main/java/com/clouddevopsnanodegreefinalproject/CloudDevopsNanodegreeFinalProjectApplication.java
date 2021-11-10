920feb04fb583827a625703e3f91365d3a869935
Jonas <jonas.tosti@telefonica.com>
2021-11-10 19:45:51 -0300

    Update CloudDevopsNanodegreeFinalProjectApplication.java

--- src/main/java/com/clouddevopsnanodegreefinalproject/CloudDevopsNanodegreeFinalProjectApplication.java
+++ src/main/java/com/clouddevopsnanodegreefinalproject/CloudDevopsNanodegreeFinalProjectApplication.java
@@ -2,9 +2,18 @@ package com.clouddevopsnanodegreefinalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CloudDevopsNanodegreeFinalProjectApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Jonas! This is running in a container ";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudDevopsNanodegreeFinalProjectApplication.class, args);
